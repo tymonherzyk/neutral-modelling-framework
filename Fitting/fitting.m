162098781f43bebf29f3358a8348285adbff7188
tymonherzyk <60410897+tymonherzyk@users.noreply.github.com>
2025-02-21 16:02:33 +0000

    Update fitting.m
    Updated comments

--- Fitting/fitting.m
+++ Fitting/fitting.m
@@ -1,66 +1,54 @@
 %fitting.m
 
-%Author: Tymon Herzyk
-%Last Editied: 05/09/2021
+%Author: Tymon Alexander Herzyk
+%Github: 
+%Last Editied: 21/02/2025
 %Version: 1.0
 %MATLAB Version: R2020b
-%License:
-%END INTRODUCTION%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%License: https://creativecommons.org/licenses/by/4.0/
 
 %START FITTING%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Clear workspace
-clear
-%Start timer
-tstartSim = tic;
-%Print start message
-fprintf('FITTING STARTED \n')
-%START SETUP%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Set import settings for loading parameters from user spreadsheet
-importFilename = 'fittingParameters.xlsx';
-opts = detectImportOptions(importFilename);
+clear %clear workspace
+tstartSim = tic; %start timer
+fprintf('FITTING STARTED \n') %print start message
+%START SETUP%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+importFilename = 'fittingParameters.xlsx'; %set import filename for loading parameters from user spreadsheet
+opts = detectImportOptions(importFilename);  %set import settings for loading parameters from user spreadsheet
 opts = setvartype(opts,'char');
 opts.RowNamesRange = 'A2';
 opts.VariableNamesRange = 'B1';
 opts.DataRange = 'B2';
 opts.Sheet = 'fitting';
-%Load parameters from user spreadsheet as table
-MainParametersTable = readtable(importFilename,opts);
-%Change parameters to correct data types
-for i = 1:height(MainParametersTable)
+MainParametersTable = readtable(importFilename,opts); %load parameters from user spreadsheet as table
+for i = 1:height(MainParametersTable) %change parameters to correct data types
     if strcmpi('number',MainParametersTable.Type{i}) 
         MainParametersTable.Value{i} = sscanf(MainParametersTable.Value{i},'%f*');
     end
 end
-%Store parameters in local data structure
-MainParameters.function = MainParametersTable.Value{1};  
+MainParameters.function = MainParametersTable.Value{1}; %store parameters in local data structure
 MainParameters.loadDataPath = MainParametersTable.Value{2};
 MainParameters.saveDataIdentifier = MainParametersTable.Value{3};
 MainParameters.saveDataPath = MainParametersTable.Value{4};
 MainParameters.saveLogPath = MainParametersTable.Value{5};
 fprintf(">>>> Select files for fitting \n")
-[MainParameters.userFileNames,MainParameters.userFilePath] = uigetfile(MainParameters.loadDataPath,"Multiselect","on");
-if ~iscell(MainParameters.userFileNames)
+[MainParameters.userFileNames,MainParameters.userFilePath] = uigetfile(MainParameters.loadDataPath,"Multiselect","on"); %open dialog box to select data files
+if ~iscell(MainParameters.userFileNames) %store filenames of selected data files
     MainParameters.userFileNames={MainParameters.userFileNames};
 end
-MainParameters.userFileTotal = length(MainParameters.userFileNames);
+MainParameters.userFileTotal = length(MainParameters.userFileNames); %store total number of selected data files
 fprintf(">>>> Files Selected: \n")
-for i = 1:MainParameters.userFileTotal
+for i = 1:MainParameters.userFileTotal %print selected files to user
     fprintf(">>>> %s \n", string(MainParameters.userFileNames(i)))
 end
 %END SETUP%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %START MAIN BODY%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%For number of runs
-for i = 1:MainParameters.userFileTotal
-    %Start timer and store date and time
-    tstartRun = tic;
+for i = 1:MainParameters.userFileTotal %for number of runs
+    tstartRun = tic; %start timer and store date and time
     dateTimeRun = datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss');
-    %Print start message 
-    fprintf('\nFILE %d OF %d STARTED \n',i,MainParameters.userFileTotal)
-    %Define file load path
-    loadDataFullPath = fullfile(MainParameters.userFilePath,MainParameters.userFileNames(i));
+    fprintf('\nFILE %d OF %d STARTED \n',i,MainParameters.userFileTotal) %print start message 
+    loadDataFullPath = fullfile(MainParameters.userFilePath,MainParameters.userFileNames(i)); %Define data file load path
     loadDataFullPath = string(loadDataFullPath);
-    %run function defined by user and pass parameters
-    if strcmpi('hubbellfit',MainParameters.function)
+    if strcmpi('hubbellfit',MainParameters.function) %run function defined by user and pass parameters
         [Results,dataFilename,Log] = hubbellfit(MainParameters,loadDataFullPath);
     elseif strcmpi('hubbellsimpfit',MainParameters.function)
         [Results,dataFilename,Log] = hubbellfit(MainParameters,loadDataFullPath);
@@ -69,20 +57,17 @@ for i = 1:MainParameters.userFileTotal
     elseif strcmpi('sloansimpfit',MainParameters.function)
         [Results,dataFilename,Log] = sloanfit(MainParameters,loadDataFullPath);
     end
-    %End timer
-    tendRun = toc(tstartRun);
+    tendRun = toc(tstartRun); %end timer
 %START SAVING DATA%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    saveDataFullPath = append(MainParameters.saveDataPath,dataFilename);
-    save(saveDataFullPath,'Results')
-    fprintf('>>>> Data Saved As: %s \n',dataFilename)
-    fprintf('>>>> Data Saved In: %s \n',MainParameters.saveDataPath)
+    saveDataFullPath = append(MainParameters.saveDataPath,dataFilename); %build full path for saving data
+    save(saveDataFullPath,'Results') %save data
+    fprintf('>>>> Data Saved As: %s \n',dataFilename) %print save filename to user
+    fprintf('>>>> Data Saved In: %s \n',MainParameters.saveDataPath) %print save location to user
 %END SAVING DATA%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%START LOGGING%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %Create filename and path for saving log
-    logFilename = sprintf('Log_%s',dataFilename);
+%START SAVING LOG%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    logFilename = sprintf('Log_%s',dataFilename); %build full path for saving data
     saveLogFullPath = append(MainParameters.saveLogPath,logFilename);
-    %Add any required variables to log
-    Log.RunParameters.originalDataFilename = MainParameters.userFileNames(i);
+    Log.RunParameters.originalDataFilename = MainParameters.userFileNames(i);  %add additonal variables to log
     Log.RunParameters.dataFilename = dataFilename;
     Log.RunParameters.dataPath = MainParameters.saveDataPath;
     Log.RunParameters.fixedParameters = extractBetween(dataFilename,'fixed','_T');
@@ -91,14 +76,13 @@ for i = 1:MainParameters.userFileTotal
     Log.RunParameters.runtime = tendRun;
     Log.RunParameters.datetime = dateTimeRun;
     Log.MainParameters = MainParameters;
-    Log = orderfields(Log);
-    %Save log
-    save(saveLogFullPath,'Log')
-    fprintf('>>>> Log Saved As: %s \n',logFilename)
-    fprintf('>>>> Log Saved In: %s \n',MainParameters.saveLogPath)
-%END LOGGING%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    fprintf('FILE %d OF %d FINISHED \n',i,MainParameters.userFileTotal)
+    Log = orderfields(Log); %order log
+    save(saveLogFullPath,'Log') %save log
+    fprintf('>>>> Log Saved As: %s \n',logFilename) %print save filename to user
+    fprintf('>>>> Log Saved In: %s \n',MainParameters.saveLogPath) %print save location to user
+%END SAVING LOGG%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    fprintf('FILE %d OF %d FINISHED \n',i,MainParameters.userFileTotal) %print end message
 end
 %END MAIN BODY%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-fprintf('\nFITTING FINISHED \n')
-%END SAMPLING%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fprintf('\nFITTING FINISHED \n') %print end message
+%END FITTING%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
