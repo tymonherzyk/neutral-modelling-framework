ce704b8652ddb5c57a5f42a13351f4e87c94fc76
tymonherzyk <60410897+tymonherzyk@users.noreply.github.com>
2025-02-23 12:05:36 +0000

    Update even.m

--- Sampling/even.m
+++ Sampling/even.m
@@ -1,78 +1,64 @@
 %even.m
 
-%Author: Tymon Herzyk
-%Last Editied: 21/08/2024
+%Author: Tymon Alexander Herzyk
+%Github: 
+%Last Editied: 21/02/2025
 %Version: 1.0
 %MATLAB Version: R2020b
-%License:
+%License: https://creativecommons.org/licenses/by/4.0/
 
 %START EVEN%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [NS,dataFilename,Log] = even(MainParameters, loadPath)
-fprintf('EVEN STARTED \n')
+function [NS,dataFilename,Log] = even(MainParameters, loadPath) %start function
+fprintf('EVEN STARTED \n') %print start message
 %START SETUP%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Set import settings for loading function parameters from user spreadsheet
-importFilename = 'samplingParameters.xlsx';
-opts = detectImportOptions(importFilename);
+importFilename = 'samplingParameters.xlsx'; %set import filename for loading parameters from user spreadsheet
+opts = detectImportOptions(importFilename); %set import settings for loading parameters from user spreadsheet
 opts = setvartype(opts,'char');
 opts.RowNamesRange = 'A2';
 opts.VariableNamesRange = 'B1';
 opts.DataRange = 'B2';
 opts.Sheet = 'even';
-%Load function parameters from user spreadsheet as table
-FunctionParametersTable = readtable(importFilename,opts);
-%Change function parameters to correct data types
-for i = 1:height(FunctionParametersTable)
+FunctionParametersTable = readtable(importFilename,opts); %load function parameters from user spreadsheet as table
+for i = 1:height(FunctionParametersTable) %change function parameters to correct data types
     if strcmpi('number',FunctionParametersTable.Type{i}) 
         FunctionParametersTable.Value{i} = sscanf(FunctionParametersTable.Value{i},'%f*');
     end
 end
-%Store function parameters in local data structure
-FunctionParameters.startTime = FunctionParametersTable.Value{1};
+FunctionParameters.startTime = FunctionParametersTable.Value{1}; %store function parameters in data structure
 FunctionParameters.endTime = FunctionParametersTable.Value{2};
 FunctionParameters.sampleFrequency = FunctionParametersTable.Value{3};   
 %END SETUP%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %START MAIN BODY%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Load NS data
-load(loadPath,'NS');
-originalNS = NS;
+load(loadPath,'NS'); %load data
+originalNS = NS; %save copy of data
 clear NS
-%Extract file naming identifier
-originalFilename = extractAfter(loadPath,MainParameters.userFilePath);
+originalFilename = extractAfter(loadPath,MainParameters.userFilePath); %extract file name
 originalDataIdentifier = extractBefore(originalFilename,'_');
-%Calculate indexing variables
-indexStartTimeArray = find(originalNS(:,2)>=FunctionParameters.startTime);
-indexStartTime = indexStartTimeArray(1,1);
-indexEndTimeArray = find(originalNS(:,2)<=FunctionParameters.endTime);
-indexEndTime = indexEndTimeArray(end,1);
-indexSampleFrequency = height(find(originalNS(:,2)>0 & originalNS(:,2)<=FunctionParameters.sampleFrequency));
-%Copy data points from original NS to new array
-NS(:,1) = originalNS(indexStartTime:indexSampleFrequency:indexEndTime, 1);
-NS(:,2) = originalNS(indexStartTime:indexSampleFrequency:indexEndTime, 2);
-%Check for decimal places in sample frequency
-dpCheck = regexp(num2str(FunctionParameters.sampleFrequency),'\.','split');
-if length(dpCheck) == 2
+indexStartTimeArray = find(originalNS(:,2)>=FunctionParameters.startTime); %find sampling start point
+indexStartTime = indexStartTimeArray(1,1); %set sampling start point
+indexEndTimeArray = find(originalNS(:,2)<=FunctionParameters.endTime); %find sampling end point
+indexEndTime = indexEndTimeArray(end,1); %set sampling end point
+indexSampleFrequency = height(find(originalNS(:,2)>0 & originalNS(:,2)<=FunctionParameters.sampleFrequency)); %calculate sampling frequency
+NS(:,1) = originalNS(indexStartTime:indexSampleFrequency:indexEndTime, 1); %copy desired data points from original NS to new array
+NS(:,2) = originalNS(indexStartTime:indexSampleFrequency:indexEndTime, 2); %copy desired time points from original NS to new array
+dpCheck = regexp(num2str(FunctionParameters.sampleFrequency),'\.','split'); %check for decimal places
+if length(dpCheck) == 2 %set number of sig figs
     dp = length(dpCheck{2});
 else
     dp = 0;
 end
 NS(:,2) = round(NS(:,2)-NS(1,2),dp);
-%Calculate total samples and total time
-totalSamples = height(NS);
-totalTime = NS(end,2);
-%Check for decimal places in total time
-dpCheck = regexp(num2str(totalTime),'\.','split');
-%Set number of sig figs for total time
-if length(dpCheck) == 2
+totalSamples = height(NS); %calculate total samples
+totalTime = NS(end,2); %calculate total time
+dpCheck = regexp(num2str(totalTime),'\.','split'); %check for decimal places
+if length(dpCheck) == 2 %set number of sig figs
     dp = length(dpCheck{2});
 else
     dp = 0;
 end
-%Create data filename
-dataFilename = sprintf('%s%s_T%.*f_S%d.mat',originalDataIdentifier, MainParameters.saveDataIdentifier,dp, totalTime, totalSamples);
-%START LOGGING%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Log.FunctionParameters = FunctionParameters;
-%END LOGGING%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dataFilename = sprintf('%s%s_T%.*f_S%d.mat',originalDataIdentifier, MainParameters.saveDataIdentifier,dp, totalTime, totalSamples); %create data filename
+Log.FunctionParameters = FunctionParameters; %log function paramaters
 %END MAIN BODY%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-fprintf('EVEN FINISHED \n')
+fprintf('EVEN FINISHED \n') %print end message
 end
 %END EVEN%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
